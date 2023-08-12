6a404ac36cf159b7beb380d84163e4e5d8fe3c83
Eric Zheng <eric.zheng@outlook.com>
2023-08-12 12:56:29 +1200

    Feat: added landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,22 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import styles from './home.module.css';
+import styles from './index.module.css';
+import Button from '@mui/material/Button';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter()
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <div
+      className={styles.main}
     >
       <div>
-        <h1 className="text-6xl font-bold">BeHealthy</h1>
-        <p className="text-2xl mt-4">Your health is our priority</p>
+        <h1 className="text-6xl font-bold text-stone-800"><span className='text-6xl font-bold text-stone-800'>Be</span>Healthy</h1>
+        <p className="text-2xl mt-1">Your health is our priority</p>
+        <Button className="mt-9" variant="contained" href="#">Sign in with Google</Button>
       </div>
-    </main>
+    </div>
   )
 }
