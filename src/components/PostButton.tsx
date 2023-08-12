027115378d1f0d8916d37f249e5b1ca952da4175
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-08-12 23:39:22 +1200

    form functionality

--- src/components/PostButton.tsx
+++ src/components/PostButton.tsx
@@ -9,7 +9,61 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
-import { Textarea } from './ui/textarea';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
+import { useToast } from '@/components/ui/use-toast';
+
+export function TextareaForm() {
+  const form = useForm();
+  const {toast} = useToast()
+  function onSubmit(data: any) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+          <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='w-full space-y-6'>
+        <FormField
+          control={form.control}
+          name='post'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea
+                  placeholder='Tell us a little bit about yourself'
+                  className='resize-none'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <DialogPrimitive.Close asChild>
+          <Button type='submit' className='w-full'>
+            Post
+          </Button>
+        </DialogPrimitive.Close>
+      </form>
+    </Form>
+  );
+}
 
 export function PostButton() {
   return (
@@ -22,11 +76,8 @@ export function PostButton() {
           <DialogTitle>Post message</DialogTitle>
         </DialogHeader>
         <div className='grid gap-4 py-4'>
-          <Textarea></Textarea>
+          <TextareaForm></TextareaForm>
         </div>
-        <DialogPrimitive.Close asChild>
-          <Button>Post</Button>
-        </DialogPrimitive.Close>
       </DialogContent>
     </Dialog>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { Toaster } from '@/components/ui/toaster';
 
 
 export default function App({ session, Component, pageProps }: any) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Toaster />
     </SessionProvider>
   );
 }
