842f05f1f77632b12ee424536866cc3d7fafc9ed
Eric Zheng <eric.zheng@outlook.com>
2023-08-13 11:25:08 +1200

    fixed sizing

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,11 +5,10 @@ import { useRouter } from 'next/router'
 import styles from './profile.module.css';
 import {useState, useEffect} from 'react'
 import defaultpfp from '../../public/images/Default_pfp.svg'
-import { Separator } from "@/components/ui/separator"
 import FriendCard from '@/components/FriendCard';
+import HomeIcon from '@mui/icons-material/Home';
 
 const inter = Inter({ subsets: ['latin'] })
-
 interface user {
   id: number,
   name: string,
@@ -33,27 +32,20 @@ export default function Home() {
       { id: 1, name: "user", post: "I am an upstanding and mindful person who is always looking to improve myself. I am a very kind person who is always looking to help others." },
       { id: 2, name: "user", post: "After years of hard work, she finally achieved her dream and felt a deep sense of fulfillment." },
       { id: 3, name: "user", post: "The laughter of children playing in the park is a reminder of the innocence and happiness that life can bring." },
+      { id: 4, name: "user", post: "The laughter of children playing in the park is a reminder of the innocence and happiness that life can bring." },
     ];
     setUser(userInfo[0]);
     setPosts(fetchedPosts);
   }, [])
 
   return (
-    <div
-      className={styles.main}
-    >
-      <div className="flex justify-between items-center w-full static">
-        <Link href="/friends"> {/* Link to Friends page */}
-            <Image
-              src="/images/friends.png"
-              alt="Friends Icon"
-              width={40}
-              height={30}
-            />
-          
+    <div className={styles.main}>
+      <div className='flex justify-between items-center w-full'>
+        <Link href="/home"> {/* Link to Friends page */}
+          <HomeIcon sx={{ fontSize: 40 }}/>
         </Link>
         <Link href="/home">
-            <h1 className="text-2xl font-bold text-foreground-rgb">BeHealthy</h1>
+          <h1 className="text-2xl font-bold text-foreground-rgb">BeMindful</h1>
         </Link>
         <Link href="/profile"> {/* Link to Profile page */}
           
@@ -66,7 +58,7 @@ export default function Home() {
           
         </Link>
       </div>
-
+      
       <h1 className="text-5xl font-bold text-stone-800 mt-10 mb-5">{user.name}</h1>
       <Image src={user.pfp || defaultpfp} className={styles.logo} alt="pfp"/>
       <p className="text-sm mt-1 w-80">{user.bio}</p>
@@ -75,7 +67,7 @@ export default function Home() {
           <h2 className="text-white text-sm">{user.streak}-day streak!</h2>
         </div>
 
-      <h2>Posts:</h2>
+      <h2 className={'text-xl'}>Posts:</h2>
 
       <div className={styles.posts}>
         {posts.map((message) => (
