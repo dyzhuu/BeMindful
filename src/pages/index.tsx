3bd4b21394f850b2c48d54af48a84d64fd36b562
hunterjreid <62681404+hunterjreid@users.noreply.github.com>
2023-08-13 12:37:38 +1200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,7 @@ export default function LoginPage() {
   const router = useRouter();
   const session = useSession();
   
-  if (session.status === 'loading') {
-    return <div>Loading...</div>;
-  }
+
   
   if (session.status === 'unauthenticated') {
     return (
@@ -18,7 +16,7 @@ export default function LoginPage() {
         <Card className='border-card shadow-none md:border-border md:shadow-sm h-[100vh] w-[100vw] md:h-fit md:min-h-[250px] md:max-w-[480px] p-10 md:p-16 md:py-24 flex justify-center items-center bg-gradient-to-br from-white-300 to-grey-400 rounded-lg'>
           <div className='flex flex-col w-[350px] text-center space-y-8'>
             <h1 className='text-3xl font-semibold tracking-tight text-black-500'>
-            🥲Log In to BeHealthy.
+            Log In to BeHealthy.
             </h1>
             <p className='text-sm text-black-500'>
               Welcome to the awesome app! You must first Please log in to continue.
